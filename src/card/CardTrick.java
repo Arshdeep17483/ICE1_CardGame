5650a642bc7fb85b439cff9d7952df0b4373344e
Arshdeep Singh <arshdeepsingh@Arshdeeps-MacBook-Air.local>
2025-05-27 22:45:18 -0400

    Final working version with modifier comment and working output

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package card;
+import java.util.Random;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -15,12 +16,35 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Random random = new Random();
+        
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            c.setValue(random.nextInt(13) + 1); 
+            c.setSuit(Card.SUITS[random.nextInt(4)]);
+            magicHand[i] = c;
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        }
+         Card luckyCard = new Card();
+        luckyCard.setValue(2);
+        luckyCard.setSuit("Clubs");
+
+        boolean cardFound = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == luckyCard.getValue() && card.getSuit().equals(luckyCard.getSuit())) {
+                cardFound = true;
+                break;
+            }
+        }
+
+       
+        if (cardFound) {
+            System.out.println("found the lucky card! (2 of Clubs)");
+        } else {
+            System.out.println(" Sorry, the lucky card (2 of Clubs) .");
         }
         
         //insert code to ask the user for Card value and suit, create their card
